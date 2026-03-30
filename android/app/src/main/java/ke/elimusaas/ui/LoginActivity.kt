5a7a4199717ed48ede82c49d88961be2d9ee2b39
Antony Mwangi <mwangiantony557@gmail.com>
2026-03-30 12:04:15 +0300

    Fix import formatting in LoginActivity.kt

--- android/app/src/main/java/ke/elimusaas/ui/LoginActivity.kt
+++ android/app/src/main/java/ke/elimusaas/ui/LoginActivity.kt
@@ -2,7 +2,7 @@ package ke.elimusaas.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.View 
 import android.view.inputmethod.EditorInfo
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
